02650fd1ac88b5a1a1da7c7594755494a91573fe
minhajul islam <mm01882390860@gmail.com>
2026-01-03 23:37:20 +0600

    show/hide Button using useState and conditional rendering

--- src/UseAndCondition.jsx
+++ src/UseAndCondition.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react"
+
 export default function Show(){
+    const [show, setShow] = useState(false);
+
+    function handleShow(){
+        setShow(!show);
+    }
+
     return(
-        <div>
-            <h1>Hello,React Learner!</h1>
+        <div style={{border:'2px solid black',
+         padding:'10px',
+          margin:'50px',
+          borderRadius:'10px',
+          backgroundColor:'lightgray',
+          textAlign:'center',
+          
+          }}>
+            {show && (
+                <div>
+                                 <h1 >Hello,React Learner!</h1>
+
+                </div>
+            )}
+            <button style={{border:'2px solid black',
+            padding:'10px',
+            
+            margin:'40px',
+            borderRadius:'10px',
+            backgroundColor:'red',
+            textAlign:'center',
+            color:'white',
+            fontWeight:'bold',
+            border: '6px solid green',
+
+            }} onClick={handleShow}
+            
+            >
+                
+                {show ? 'Hide' : 'Show'} Details</button>
         </div>
     )
 }
